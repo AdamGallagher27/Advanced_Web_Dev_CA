216d7636f29e3133d7d47f096ad3850ec62efc55
Adam <N00211418@iadt.ie>
2022-10-22 17:03:12 +0100

    added create button to index

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -9,7 +9,10 @@
     </x-slot>
 
     <div class="py-12">
+       
+        
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('movies.create') }}" class="btn-link btn-lg mb-2">+ New Movie</a>
             @forelse ($movies as $movie)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 mt-6 shadow-sm sm:rounded-lg">
                 {{ $movie->title }}
