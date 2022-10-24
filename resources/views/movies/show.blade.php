8989df66cfc2fa5c1bb9606e6cc3273ee9991692
Adam <n00211418@iadt.ie>
2022-10-24 14:10:03 +0100

    added edit button

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -7,6 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{-- edit button sends movie to edit route (with movie variable)--}}
+            <a href="{{ route('movies.edit', $movie) }}" class="btn-link btn-lg mb-2">Edit</a>
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                 {{-- testing to see if movie variable is passed in --}}
                 {{-- {{ $movie }} --}}
