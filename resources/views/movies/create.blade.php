813a6f0fbe9f5a075a836a552cbb459235027289
Adam <n00211418@iadt.ie>
2022-10-24 15:58:02 +0100

    added file upload to create form

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -2,19 +2,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                <form action="{{ route('movies.store'), 'movie'  }} " method="POST">
+                <form action="{{ route('movies.store'), 'movie'  }} " method="POST" enctype="multipart/form-data">
 
                     {{-- every form needs this to prevent attacks --}}
                     @csrf
                     <x-text-input type="text" name="title" class="w-full " placeholder="Title"></x-text-input>
                     <x-text-input type="text" name="director" class="w-full " placeholder="Director"></x-text-input>
-                    <x-textarea name="description" rows="10" class="w-full mt-6" placeholder="Description..."></x-textarea>
+                    <x-textarea name="description" rows="10" class="w-full" placeholder="Description..."></x-textarea>
 
-                    {{-- image is a string right now will fix this later --}}
-                    <x-textarea name="image" rows="10" class="w-full mt-6" placeholder="image..."></x-textarea>
-                    <x-text-input type="text" name="budget" class="w-full " placeholder="budget"></x-text-input>
+                    
+
+                    <x-text-input type="text" name="budget" class="w-full" placeholder="budget"></x-text-input>
                     <x-text-input type="text" name="box_office" class="w-full " placeholder="box office"></x-text-input>
-
+                    {{-- select image --}}
+                    <p class="mt-6">Select Image</p>
+                    <x-file-input type="file" placeholder="Image" name="image" class="w-full mt-6" field="image"></x-file-input>
                     <x-primary-button name="submit" type="Submit" class="mt-6">Save Movie</x-primary-button>
                     <input type="hidden" name="_token" value="{{ Session::token() }}"> 
                 </form>
