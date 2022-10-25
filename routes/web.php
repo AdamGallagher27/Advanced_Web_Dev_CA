d3ac103021b717daf5924487110d445566854a09
Adam <N00211418@iadt.ie>
2022-10-25 19:18:06 +0100

    dashboard redirects to index

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/../index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
