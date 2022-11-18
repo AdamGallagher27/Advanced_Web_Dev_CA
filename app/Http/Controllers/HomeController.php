782dba1169810620ba7b15fc51952739c5f1ade2
Adam <n00211418@iadt.ie>
2022-11-18 10:42:45 +0000

    created home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+// checks what role the user has and sends them to the right index
+class HomeController extends Controller
+{
+    public function __construct() {
+        $this->middleware("auth");
+    }
+
+    public function index(Request $request) {
+        $user = Auth::user();
+        $home = "home";
+
+        if($user->hasRole("admin")) {
+            $home = "admin.movies.index";
+        }
+        else if($user->hasRole("user")) {
+            $home = "user.movies.index";
+        }
+        return redirect()->route($home);
+    }
+    
+}
