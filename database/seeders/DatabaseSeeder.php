e1dced70ebab5158fef55349a2fdd63f4d3143ee
Adam <N00211418@iadt.ie>
2022-11-19 16:07:00 +0000

    added production seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
 
-        
-        $this->call(MovieSeeder::class);
+        $this->call(ProductionSeeder::class);
     }
 }
