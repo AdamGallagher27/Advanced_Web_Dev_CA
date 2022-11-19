424ee54efc8d739ff26e1db89b8a47d5243aa21e
Adam <N00211418@iadt.ie>
2022-11-19 13:04:53 +0000

    added error messages to form

--- resources/views/Admin/movies/create.blade.php
+++ resources/views/Admin/movies/create.blade.php
@@ -6,16 +6,37 @@
 
                     {{-- every form needs this to prevent attacks --}}
                     @csrf
+
+                    @error('title')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <x-text-input type="text" name="title" class="w-full " placeholder="Title"></x-text-input>
+
+                    @error('director')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <x-text-input type="text" name="director" class="w-full " placeholder="Director"></x-text-input>
+
+                    @error('description')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <x-textarea name="description" rows="10" class="w-full" placeholder="Description..."></x-textarea>
 
-                    
-
+                    @error('budget')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <x-text-input type="text" name="budget" class="w-full" placeholder="budget"></x-text-input>
+
+                    @error('box_office')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <x-text-input type="text" name="box_office" class="w-full " placeholder="box office"></x-text-input>
+
                     {{-- select image --}}
                     <p class="mt-6">Select Image</p>
+                    @error('image')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <x-file-input type="file" placeholder="Image" name="image" class="w-full mt-6" field="image"></x-file-input>
                     <x-primary-button name="submit" type="Submit" class="mt-6">Save Movie</x-primary-button>
                     <input type="hidden" name="_token" value="{{ Session::token() }}"> 
--- resources/views/Admin/movies/edit.blade.php
+++ resources/views/Admin/movies/edit.blade.php
@@ -1,38 +1,62 @@
-
-
-
-
-
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+
                 {{-- variable for testing if $movie is coming through --}}
                 {{-- {{ $movie }} --}}
 
                 {{-- sends updated movie to update function --}}
-                <form action="{{ route('admin.movies.update', $movie), 'movie'  }} " method="POST">
+                <form action="{{ route('admin.movies.update', $movie), 'movie' }} " method="POST">
 
                     {{-- every form needs this to prevent attacks --}}
                     @csrf
 
                     {{-- form method cant have a put method --}}
                     {{-- this lets us have a put method --}}
-                    @method("put")
+                    @method('put')
 
-                    <x-text-input type="text" name="title" class="w-full " placeholder="Title" :value="@old('title', $movie->title)"></x-text-input>
-                    <x-text-input type="text" name="director" class="w-full " placeholder="Director" :value="@old('director', $movie->director)"></x-text-input>
-                    <x-textarea name="description" rows="10" class="w-full mt-6" placeholder="Description..." :value="@old('description', $movie->description)"></x-textarea>
+                    @error('title')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-text-input type="text" name="title" class="w-full"
+                        placeholder="Title" :value="@old('title', $movie->title)">
+                    </x-text-input>
 
-                    {{-- image is a string right now will fix this later --}}
-                    <x-textarea name="image" rows="10" class="w-full mt-6" placeholder="image..." :value="@old('image', $movie->image)"></x-textarea>
-                    <x-text-input type="text" name="budget" class="w-full " placeholder="budget" :value="@old('budget', $movie->budget)"></x-text-input>
-                    <x-text-input type="text" name="box_office" class="w-full " placeholder="box office" :value="@old('box_office', $movie->box_office)"></x-text-input>
+                    @error('director')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-text-input type="text" name="director" class="w-full " placeholder="Director"
+                        :value="@old('director', $movie->director)"></x-text-input>
+
+                    @error('description')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-textarea name="description" rows="10" class="w-full mt-6" placeholder="Description..."
+                        :value="@old('description', $movie->description)"></x-textarea>
+
+                    @error('image')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-textarea name="image" rows="10" class="w-full mt-6" placeholder="image..."
+                        :value="@old('image', $movie->image)"></x-textarea>
+
+                    @error('budget')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-text-input type="text" name="budget" class="w-full " placeholder="budget" :value="@old('budget', $movie->budget)">
+                    </x-text-input>
+
+                    @error('box_office')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-text-input type="text" name="box_office" class="w-full " placeholder="box office"
+                        :value="@old('box_office', $movie->box_office)"></x-text-input>
 
                     <x-primary-button name="submit" type="Submit" class="mt-6">Save Movie</x-primary-button>
                     <input type="hidden" name="_token" value="{{ Session::token() }}">
                 </form>
-                
+
             </div>
         </div>
     </div>
