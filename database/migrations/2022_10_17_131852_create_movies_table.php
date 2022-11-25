9186aee4062e468d2d21f1ffb1135c03dabf0f10
Adam <n00211418@iadt.ie>
2022-11-25 11:55:26 +0000

    removed director from movie migration

--- database/migrations/2022_10_17_131852_create_movies_table.php
+++ database/migrations/2022_10_17_131852_create_movies_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamps();
             $table->foreignId("user_id")->constrained();
             $table->string('title');
-            $table->string('director');
+            // $table->string('director');
             $table->text('description');
             $table->string('image');
             $table->integer('budget');
