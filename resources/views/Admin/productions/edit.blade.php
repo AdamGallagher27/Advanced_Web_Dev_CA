d5dbc74349135026d1414a1658aba0a16c88b061
Adam <N00211418@iadt.ie>
2022-11-30 21:17:58 +0000

    created edit form

---
+++ resources/views/Admin/productions/edit.blade.php
@@ -0,0 +1,26 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                <form action="{{ route('admin.productions.update', $production), "production"  }}" method="POST">
+
+                    {{-- every form needs this to prevent attacks --}}
+                    @csrf
+
+                    {{-- form method cant have a put method --}}
+                    {{-- this lets us have a put method --}}
+                    @method('put')
+
+                    @error('title')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
+                    <x-text-input type="text"  name="title" class="w-full " placeholder="Title" :value="@old('title', $production->title)"></x-text-input>
+
+                    <x-primary-button name="submit" type="Submit" class="mt-6">Save Production</x-primary-button>
+                    <input type="hidden" name="_token" value="{{ Session::token() }}"> 
+                </form>
+                
+            </div>
+        </div>
+    </div>
+</x-app-layout>
