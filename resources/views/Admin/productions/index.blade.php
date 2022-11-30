14b124c71cd4688ff7442cb0a5cf8329c3efe685
Adam <N00211418@iadt.ie>
2022-11-30 19:59:14 +0000

    added create button

--- resources/views/Admin/productions/index.blade.php
+++ resources/views/Admin/productions/index.blade.php
@@ -10,8 +10,9 @@
     </x-slot>
 
     <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+            <a href="{{ route('admin.productions.create') }}" class="btn-link btn-lg mb-2">+ New Production Company</a>
             @foreach ($productions as $prod)
             <div class="my-6 p-6 bg-white border-b border-gray-200 mt-6 shadow-sm sm:rounded-lg">
                 <h2 class="font-bold text-2xl">
