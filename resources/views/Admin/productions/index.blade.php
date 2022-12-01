ff817e815a0f72f2c046a68e50d90c3e0f4fd00b
Adam <N00211418@iadt.ie>
2022-12-01 00:19:42 +0000

    display success if its in the session

--- resources/views/Admin/productions/index.blade.php
+++ resources/views/Admin/productions/index.blade.php
@@ -8,6 +8,14 @@
     <div class="py-12">
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            {{-- show success message for update / create --}}
+            @if (session('success'))
+                <div class="mb-4 px-4 py-2 bg-green-100 border border-green-200 text-green">
+                    {{ session('success') }}
+                </div>
+            @endif
+
             <a href="{{ route('admin.productions.create') }}" class="btn-link btn-lg mb-2">+ New Production Company</a>
             @foreach ($productions as $prod)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 mt-6 shadow-sm sm:rounded-lg">
