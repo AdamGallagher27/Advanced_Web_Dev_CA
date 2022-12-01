baa1f00aeeb391de325dcc9bc097627453f2e976
Adam <N00211418@iadt.ie>
2022-12-01 00:00:10 +0000

    added soft delete to model

--- app/Models/Production.php
+++ app/Models/Production.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Production extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     // this is the relationship for movies / production
     public function movies() {
