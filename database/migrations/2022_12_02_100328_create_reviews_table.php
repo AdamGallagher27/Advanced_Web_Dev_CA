b4d91847f90c2f84af4601456fa9b46b8d41d145
Adam <n00211418@iadt.ie>
2022-12-02 11:09:23 +0000

    created review migration

---
+++ database/migrations/2022_12_02_100328_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("description");
+            $table->integer("rating");
+
+            $table->unsignedBigInteger("movie_id");
+            $table->foreign("movie_id")->references("id")->on("movies")->onUpdate("cascade")->onDelete("restrict");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
