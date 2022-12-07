5e63bbfee518a439cc4616398a5d10534cde5ca8
Adam <N00211418@iadt.ie>
2022-12-07 16:54:42 +0000

    added store function

--- app/Http/Controllers/Reviewer/ReviewController.php
+++ app/Http/Controllers/Reviewer/ReviewController.php
@@ -26,21 +26,51 @@ class ReviewController extends Controller
      */
 
 
-    public function create( $movie_id)
+    public function create()
     {
+
+
+        // get the movie id from the get array
+        $movie_id = $_GET["movie_id"];
+
 
         // authorize user as admin
         $user = Auth::user();
         $user->authorizeRoles("reviewer");
 
-
-
         // returns view for create form
         // before its sent to the store function
-        return redirect("Reviewer.reviews.create")->with("movie_id", $movie_id);
+        return view("Reviewer.reviews.create")->with("movie_id", $movie_id);
     }
 
+    public function store(Request $request)
+    {
 
+
+        // validating form input from create()
+        $request->validate( [
+            "title" => "required|max:120",
+            "description" => "required|max:200",
+            "rating" => "required",
+            "movie_id" => "required|numeric",
+        ]);
+
+
+
+        // // adding data to review table
+        $review = Review::create([
+            "movie_id" => $request->movie_id,
+            "user_id" => Auth::id(),
+            "title" => $request->title,
+            "description" => $request->description,
+            "rating" => $request->rating,
+        ]);
+
+
+        // return to movies show
+        return redirect("reviewer/movies/" . $request->movie_id)->with('success', 'your review was created successfully');
+        
+    }
 
     
 
