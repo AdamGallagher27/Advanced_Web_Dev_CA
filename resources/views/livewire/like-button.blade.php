a246e13e3cb44562047f0762382ddce2feb6101f
Adam <N00211418@iadt.ie>
2022-12-08 16:23:33 +0000

    like button component displays like button

--- resources/views/livewire/like-button.blade.php
+++ resources/views/livewire/like-button.blade.php
@@ -1,5 +1,12 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-    <button wire:click="increment">+</button>
-    <h1>{{ $count }}</h1>
+
+    {{-- if the post has not been liked display like button --}}
+    @if (!self::hasLiked($movie->id, $currentUser->id))
+        <button wire:click="likeDislike({{ $movie->id }}, {{ $currentUser->id }})" class="btn-link mb-2 mt-3">Like Button</button>
+    
+    {{-- else display dislike button --}}
+    @else
+        <button wire:click="likeDislike({{ $movie->id }}, {{ $currentUser->id }})" class="btn-link btn-danger mb-2 mt-3">Dislike Button</button>
+    @endif
+    
 </div>
