effce1d64caa650044532a4c74313d34f48f3f0f
Adam <N00211418@iadt.ie>
2022-12-08 11:04:38 +0000

    if new user has no role make them normal user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\UserRole;
+
 
 // checks what role the user has and sends them to the right index
 class HomeController extends Controller
@@ -24,6 +27,15 @@ class HomeController extends Controller
         }
         else if($user->hasRole("reviewer")) {
             $home = "reviewer.movies.index";
+        }
+        // else the user is new and does not have a role
+        else {
+            $userRole = new UserRole;
+            $userRole->user_id = $user->id;
+            $userRole->role_id = 2;
+            $userRole->save();
+
+            $home = "user.movies.index";
         }
         return redirect()->route($home);
     }
