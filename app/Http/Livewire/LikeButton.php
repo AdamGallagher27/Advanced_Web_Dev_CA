49ead73ff0bcbb0518f065626ab0ab530a8bc2fb
Adam <N00211418@iadt.ie>
2022-12-08 16:22:36 +0000

    created like button controller

--- app/Http/Livewire/LikeButton.php
+++ app/Http/Livewire/LikeButton.php
@@ -3,18 +3,50 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
+use App\Models\Like;
 
 class LikeButton extends Component
 {
-
-    public $count = 0;
- 
     
-    public function increment()
-    {
-        $this->count++;
+
+    public $movie;
+    public $currentUser;
+
+    // checks if the user has liked the movie already
+    private function hasLiked( $movie_id, $user_id ) {
+        $likes = Like::where("movie_id", $movie_id)->get();
+
+        foreach ($likes as $like) {
+            if($like->movie_id == $movie_id and $like->user_id == $user_id) {
+                return true;
+            }
+        } 
+
+        return false;
     }
 
+    // function for liking a movie
+    // this is called when the like button is pressed on show view
+    public function likeDislike( $movie_id, $user_id )
+    {
+
+        // if the user hasnt liked this movie before
+        if(self::hasLiked($movie_id, $user_id)) {
+            // remove like from db
+            DB::table('likes')->where('movie_id', $movie_id)->where('user_id', $user_id)->delete();
+            
+        }
+        else {
+            // add new like to the like table
+            $like = new Like;
+            $like->movie_id = $movie_id;
+            $like->user_id = $user_id;
+            $like->save(); 
+        }
+
+
+    }
 
 
     // renders component to the screen
