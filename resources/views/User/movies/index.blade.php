3ef37e8d705872b963bb37f1025bb70743abc004
Adam <N00211418@iadt.ie>
2022-12-08 17:54:30 +0000

    added button for liked movies view

--- resources/views/User/movies/index.blade.php
+++ resources/views/User/movies/index.blade.php
@@ -10,8 +10,8 @@
 
     <div class="py-12">
        
-        
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('user.likedMovies') }}" class="btn-link mb-2 mt-5">favourite movies</a>
             @forelse ($movies as $movie)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 mt-6 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-2xl">
